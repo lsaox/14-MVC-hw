984c7c5f77dd076a6068c713dfe97abcef8334b1
unknown <el_word9@yahoo.com>
2024-02-28 20:54:58 -0600

    added new comment and delete comment

--- Main/controllers/api/postRoutes.js
+++ Main/controllers/api/postRoutes.js
@@ -50,3 +50,32 @@ router.delete('/:id', apiGuard, async (req, res) => {
 });
 
 module.exports = router;
+// delete a post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create a new comment
+router.post("/", withAuth, async (req, res) => {
+  const body = req.body;
+  try {
+    const newComment = await Comment.create({ ...body, userId: req.session.user_id });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
